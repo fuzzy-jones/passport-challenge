b2639482b84af6728c07bad92f1005e939c735a9
fuzzy <donald.jones83@gmail.com>
2018-07-14 20:56:24 -0400

    host mongodb on mlab, and connect to mongodb with mongoose, config/keys file

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+    'mongodb://fuzzy:Zombie666@ds137611.mlab.com:37611/passportchallenge'
+}
--- server.js
+++ server.js
@@ -1,30 +1,44 @@
 // Dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+// var bodyParser = require("body-parser");
 
 // Initialize Express
 var app = express();
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// DB config
+var db = require('./config/keys').mongoURI;
+
+// connect to mongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('mongodb connected'))
+    .catch(err => console.log(err));
+
+app.get('/', function(req, res) {
+    res.send('hello world');   
+});
+
+// // Sets up the Express app to handle data parsing
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 // port
 var port = process.env.PORT || 8080; 
 
 
-var router = express.Router();
+// var router = express.Router();
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// router.get('/', function(req, res) {
+//     res.json({ message: 'hooray! welcome to our api!' });   
+// });
 
-// more routes for our API will happen here
+// // more routes for our API will happen here
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+// // REGISTER OUR ROUTES -------------------------------
+// // all of our routes will be prefixed with /api
+// app.use('/api', router);
 
 
 // START THE SERVER

2c901df5640c3a5ccb3e1a416781e0128bf53a4a
fuzzy <donald.jones83@gmail.com>
2018-07-21 23:25:38 -0400

    authorization components, sign up and log in forms, some styling

--- client/src/components/authorization/Login.js
+++ client/src/components/authorization/Login.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react'
+import "./styles/Login.css"; 
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        };
+    }
+
+    onChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+
+        }
+
+        console.log(user);
+    }
+
   render() {
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+        <div className="login">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 m-auto">
+                    <h1>Log in</h1>
+                    <p>Sign in to your account to access the tree</p>
+                        <form onSubmit={this.onSubmit.bind(this)}>
+                            <div className="form-group">
+                                <input type="email" className="form-control" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange.bind(this)}/>
+                            </div>
+                            <div className="form-group">
+                                <input type="password" className="form-control" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange.bind(this)}/>
+                            </div>
+                            <button type="submit" className="btn btn-lg btn-primary" id="login-submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
   }
 }
--- client/src/components/authorization/Register.js
+++ client/src/components/authorization/Register.js
@@ -1,13 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./styles/Register.css"; 
 
 class Register extends Component {
-  render() {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            password2: '',
+            errors: {}
+        };
+    }
+
+    onChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        }
+
+        console.log(newUser);
+    }
+    
+    render() {
     return (
-      <div>
-        <h1>Register</h1>
-      </div>
+        <div className="register">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 m-auto">
+                    <h1>Sign Up</h1>
+                    <p>Create account to access the tree</p>
+                        <form onSubmit={this.onSubmit.bind(this)}>
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange.bind(this)}/>
+                            </div>
+                            <div className="form-group">
+                                <input type="email" className="form-control" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange.bind(this)}/>
+                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <div className="form-group">
+                                <input type="password" className="form-control" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange.bind(this)}/>
+                            </div>
+                            <div className="form-group">
+                                <input type="password" className="form-control" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange.bind(this)}/>
+                            </div>
+                            <button type="submit" className="btn btn-lg btn-primary" id="register-submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
-  }
+    }
 }
 
 export default Register;

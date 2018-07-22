411dd0c08109f2532b9c2a6cbece856fc12c0c5a
fuzzy <donald.jones83@gmail.com>
2018-07-22 00:14:34 -0400

    register client validation, axios to test api to send user to DB

--- client/src/components/authorization/Register.js
+++ client/src/components/authorization/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 import "./styles/Register.css"; 
 
 class Register extends Component {
@@ -27,10 +29,17 @@ class Register extends Component {
             password2: this.state.password2
         }
 
-        console.log(newUser);
+        // console.log(newUser);
+        axios.post('/api/users/register', newUser)
+            .then(res => console.log(res.data))
+            // set the errors to the errors object state
+            .catch(err => this.setState({ errors: err.response.data }));
     }
     
     render() {
+        // const errors = this.state.errors;
+        const { errors } = this.state;
+
     return (
         <div className="register">
             <div className="container">
@@ -40,17 +49,22 @@ class Register extends Component {
                     <p>Create account to access the tree</p>
                         <form onSubmit={this.onSubmit.bind(this)}>
                             <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Name" name="name" value={this.state.name} onChange={this.onChange.bind(this)}/>
+                                <input type="text" className={classnames("form-control", {'is-invalid': errors.name})} placeholder="Name" name="name" value={this.state.name} onChange={this.onChange.bind(this)}/>
+                                {/* display the errors.name from the server validation to the client */}
+                                {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
                             </div>
                             <div className="form-group">
-                                <input type="email" className="form-control" placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange.bind(this)}/>
+                                <input type="email" className={classnames("form-control", {'is-invalid': errors.email})} placeholder="Email Address" name="email" value={this.state.email} onChange={this.onChange.bind(this)}/>
+                                {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
                             <div className="form-group">
-                                <input type="password" className="form-control" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange.bind(this)}/>
+                                <input type="password" className={classnames("form-control", {'is-invalid': errors.password})} placeholder="Password" name="password" value={this.state.password} onChange={this.onChange.bind(this)}/>
+                                {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                             </div>
                             <div className="form-group">
-                                <input type="password" className="form-control" placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange.bind(this)}/>
+                                <input type="password" className={classnames("form-control", {'is-invalid': errors.password2})} placeholder="Confirm Password" name="password2" value={this.state.password2} onChange={this.onChange.bind(this)}/>
+                                {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
                             </div>
                             <button type="submit" className="btn btn-lg btn-primary" id="register-submit">Submit</button>
                         </form>
